1106bf2cdc455cfcf16ed954ea2cddb32cb8312d
mahady13 <mohiuddinmahady@gmail.com>
2026-07-24 06:52:29 +0600

    started the function:making a giant function for all

--- assets.py
+++ assets.py
@@ -6,3 +6,19 @@ import torchvision.transforms as transforms
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import streamlit as st
+
+@st.cache_resource
+def load_asset():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = models.mobilenet_v2(weights=None)
+    model.classifier = nn.Sequential(
+        nn.Dropout(p=0.5),
+        nn.Linear(1280, 128),
+        nn.ReLU(),
+        nn.Dropout(p=0.3),
+        nn.Linear(128, 2),
+    )
+    checkpoint = torch.load("best_model.pth", map_location=device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.eval()
+    model = model.to(device)
