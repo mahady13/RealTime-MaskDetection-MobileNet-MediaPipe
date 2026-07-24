c2646381927da5e66acd2256b9f3ce75d72c68e0
mahady13 <mohiuddinmahady@gmail.com>
2026-07-24 06:53:26 +0600

    completed building the assets.py

--- assets.py
+++ assets.py
@@ -22,3 +22,23 @@ def load_asset():
     model.load_state_dict(checkpoint["model_state_dict"])
     model.eval()
     model = model.to(device)
+
+    label_encoder=joblib.load("label_encoder.pkl")
+
+    base_options = python.BaseOptions(model_asset_path='face_detector.tflite')
+    options = vision.FaceDetectorOptions(
+        base_options=base_options,
+        min_detection_confidence=0.5
+    )
+    detector = vision.FaceDetector.create_from_options(options=options)
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    return device,label_encoder,detector,transform,model
